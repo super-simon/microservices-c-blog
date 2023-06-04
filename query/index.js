b1dce91f2bb8b8cb31aa55e00bb13cf2636c9fb6
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-06-04 11:59:41 +0300

    query blank

---
+++ query/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {});
+
+app.post("/events", (req, res) => {});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
